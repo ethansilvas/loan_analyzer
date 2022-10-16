42e206e72369ff40349c8f4f08995ca632230770
Ethan Silvas <ethansilvas@gmail.com>
2022-10-16 16:11:27 -0700

    part 4: inexpensive loans

--- GitHub_Upload/loan_analyzer.py
+++ GitHub_Upload/loan_analyzer.py
@@ -163,13 +163,17 @@ loans = [
 ]
 
 # @TODO: Create an empty list called `inexpensive_loans`
-# YOUR CODE HERE!
+inexpensive_loans = []
 
 # @TODO: Loop through all the loans and append any that cost $500 or less to the `inexpensive_loans` list
-# YOUR CODE HERE!
+for loan in loans:
+    price = loan.get('loan_price')
+
+    if price <= 500:
+        inexpensive_loans.append(loan)
 
 # @TODO: Print the `inexpensive_loans` list
-# YOUR CODE HERE!
+print(f'All inexpensive loans: {inexpensive_loans}')
 
 
 """Part 5: Save the results.
